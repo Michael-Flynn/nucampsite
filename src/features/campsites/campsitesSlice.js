cd2edd2f38d713ab8feb8ff2fb588246f215b792
Michael Flynn <mchltflnn@gmail.com>
2022-08-24 05:56:23 -0500

    useParams

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,11 +1,15 @@
 import { CAMPSITES } from '../../app/shared/CAMPSITES'
 
 export const selectAllCampsites = () => {
-  return CAMPSITES
+  return CAMPSITES;
 }
 
 // export const selectRandomCampsite = () => {
 //   return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())]
 // }
 
-export const selectCampsiteById = (id) => { return CAMPSITES.find((campsite) => campsite.id === id) }
+export const selectCampsiteById = (id) => { return CAMPSITES.find((campsite) => campsite.id === parseInt(id)) }
+
+export const selectFeaturedCampsite = () => {
+  return CAMPSITES.find((campsite) => campsite.featured);
+};
