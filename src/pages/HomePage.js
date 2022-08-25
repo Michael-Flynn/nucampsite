5aecacdf0da98997f930f6e86bbd7ce9488e1631
Michael Flynn <mchltflnn@gmail.com>
2022-08-24 19:36:59 -0500

    HomePage fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,14 @@
-const HomePage =() => {  
-    return <div>HomePage</div>
-    };
+import { Container } from 'reactstrap'
+// import DisplayList from '../features/display/DisplayList'
+import SubHeader from '../components/SubHeader'
 
-    export default HomePage;
+const HomePage = () => {
+  return (
+    <Container>
+      <SubHeader current='Home' />
+      {/* <DisplayList /> */}
+    </Container>
+  );
+};
+
+export default HomePage;
