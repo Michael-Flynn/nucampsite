0105612ef7632520a1a23c05187f80493ad3788e
Michael Flynn <mchltflnn@gmail.com>
2022-09-10 11:29:42 -0500

    UserLoginError

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { commentsReducer } from '../features/comments/commentSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { userReducer } from '../features/users/userSlice';
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +12,7 @@ export const store = configureStore({
     comments: commentsReducer,
     partners: partnersReducer,
     promotions: promotionsReducer,
+    users: userReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
